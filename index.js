748da7a22bf5d66e734e3c20a7dbaba0b9f59cc4
lucasferreira <panchorf@gmail.com>
2018-04-20 18:05:35 -0300

    - Bump version

--- index.js
+++ index.js
@@ -2,22 +2,22 @@
  * @providesModule AndroidPermissions
  */
 
-'use strict';
+"use strict";
 
-import { Platform, NativeModules } from 'react-native';
+import { Platform, NativeModules } from "react-native";
 
 export function checkPermission(perm) {
-    return NativeModules.RNPermissionsAndroid.checkPermission(perm)
-};
+  return NativeModules.RNPermissionsAndroid.checkPermission(perm);
+}
 
 export function requestPermission(perm) {
-    if(typeof perm == "string") perm = [perm];
+  if (typeof perm == "string") perm = [perm];
 
-    // permCode greater than 16 bits causes an error so limit code to max of 65535
-    let maximum = 65535;
-    let minimum = 1;
+  // permCode greater than 16 bits causes an error so limit code to max of 65535
+  let maximum = 65535;
+  let minimum = 1;
 
-    let permCode = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
+  let permCode = Math.floor(Math.random() * (maximum - minimum + 1)) + minimum;
 
-    return NativeModules.RNPermissionsAndroid.requestPermission(perm, permCode);
-};
+  return NativeModules.RNPermissionsAndroid.requestPermission(perm, permCode);
+}
